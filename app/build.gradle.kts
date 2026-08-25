326309a934044f9222d1a8bfca278e945d0597fa
hocti <hocti@hotmail.com>
2026-08-25 17:50:20 +0100

    chore: 版本 1.1.1 (versionCode 18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "hk.tq9"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.0"
+        versionCode = 18
+        versionName = "1.1.1"
     }
 
     signingConfigs {
