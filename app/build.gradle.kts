2771b91e57124349e0fdfdf950ba9b48b22d3f4e
hocti <hocti@hotmail.com>
2026-08-28 02:18:33 +0100

    chore: 版本 1.1.8 (versionCode 25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "hk.tq9"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.1.7"
+        versionCode = 25
+        versionName = "1.1.8"
     }
 
     signingConfigs {
