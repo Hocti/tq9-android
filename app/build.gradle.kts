1b1bc105b0d347ee0a9d5696ad9a65d4c14493f0
hocti <hocti@hotmail.com>
2026-08-29 00:22:37 +0100

    chore: 版本 1.2.9 (versionCode 36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "hk.tq9"
         minSdk = 26
         targetSdk = 36
-        versionCode = 35
-        versionName = "1.2.8"
+        versionCode = 36
+        versionName = "1.2.9"
     }
 
     signingConfigs {
